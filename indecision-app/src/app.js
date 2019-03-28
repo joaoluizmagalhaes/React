bd5396c97984e577713bf323e7fdf7b988a0a2a4
João Magalhães <joao.magalhaes@newcontent.com.br>
2019-03-28 17:03:17 -0300

    Section 3 - Lesson 18, forms and inputs.

--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -3,49 +3,44 @@ console.log("App.js is running!");
 const app = {
 	title: 'Indecision App',
 	subtitle: 'Subtitle',
-	options: ['One', 'Two']
+	options: []
 };
 
-//JSX - JavaScript XML
-const template = (
-	<div>
-		<h1>{app.title}</h1>
-		{app.subtitle && <p>{app.subtitle}</p>}
-		<p>{app.options.length > 0 ? 'Here are your options: ' : 'No options'}</p>
-		<ol>
-			<li>Item one</li>
-			<li>Item two</li>
-		</ol>
-	</div>
-);
+const onFormSubmit = (e) => {
+	e.preventDefault();
 
-let count = 0;
-const addOne = () => {
-	count++;
-	renderCounterApp();
+	const option = e.target.elements.option.value;
+
+	if(option) {
+		app.options.push(option);
+		e.target.elements.option.value = '';
+		render();
+	}
+	
 }
-const minusOne = () => {
-	count--;
-	renderCounterApp();
-}
-const reset = () => {
-	count = 0;
-	renderCounterApp();
-}
+
+//JSX - JavaScript XML
 
 const appRoot = document.getElementById('app');
 
-const renderCounterApp = () => {
-	const templateTwo = (
+const render = () => {
+	const template = (
 		<div>
-			<h1>Count: {count}</h1>
-			<button onClick={addOne}>+1</button>
-			<button onClick={minusOne}>-1</button>
-			<button onClick={reset}>Reset</button>		
+			<h1>{app.title}</h1>
+			{app.subtitle && <p>{app.subtitle}</p>}
+			<p>{app.options.length > 0 ? 'Here are your options: ' : 'No options'}</p>
+			<p>{app.options.length}</p>
+			<ol>
+				<li>Item one</li>
+				<li>Item two</li>
+			</ol>
+			<form onSubmit={onFormSubmit}>
+				<input type="text" name="option"/>
+				<button>Add Option</button>
+			</form>
 		</div>
 	);
+	ReactDOM.render(template, appRoot);
+}
 
-	ReactDOM.render(templateTwo, appRoot);
-};
-
-renderCounterApp();
+render();
