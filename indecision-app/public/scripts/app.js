451ad8799c69544acef6bba6e01c75c019d8e878
João Magalhães <joao.magalhaes@newcontent.com.br>
2019-03-28 14:58:25 -0300

    Section 3 - Lesson 14, ES6 Aside Arrow Functions.

--- indecision-app/public/scripts/app.js
+++ indecision-app/public/scripts/app.js
@@ -1,80 +1,25 @@
 'use strict';
 
-console.log("App.js is running!");
-
-var app = {
-	title: 'Indecision App',
-	subtitle: 'Subtitle',
-	options: ['One', 'Two']
+var square = function square(x) {
+    return x * x;
 };
 
-//JSX - JavaScript XML
-var template = React.createElement(
-	'div',
-	null,
-	React.createElement(
-		'h1',
-		null,
-		app.title
-	),
-	app.subtitle && React.createElement(
-		'p',
-		null,
-		app.subtitle
-	),
-	React.createElement(
-		'p',
-		null,
-		app.options.length > 0 ? 'Here are your options: ' : 'No options'
-	),
-	React.createElement(
-		'ol',
-		null,
-		React.createElement(
-			'li',
-			null,
-			'Item one'
-		),
-		React.createElement(
-			'li',
-			null,
-			'Item two'
-		)
-	)
-);
-var user = {
-	name: 'Joao Luiz Magalhães',
-	age: 36,
-	location: 'São Paulo'
+// const squareArrow = (x) => {
+//     return x * x;
+// };
+
+var squareArrow = function squareArrow(x) {
+    return x * x;
 };
 
-function getLocation(location) {
-	if (location) {
-		return React.createElement(
-			'p',
-			null,
-			'Location: ',
-			location
-		);
-	}
-}
+console.log(squareArrow(8));
 
-var templateTwo = React.createElement(
-	'div',
-	null,
-	React.createElement(
-		'h1',
-		null,
-		user.name ? user.name : 'Anonymous'
-	),
-	user.age && user.age >= 18 && React.createElement(
-		'p',
-		null,
-		'Age: ',
-		user.age
-	),
-	getLocation(user.location)
-);
-var appRoot = document.getElementById('app');
+// const getFirstName = (fullName) => {
+//     return fullName.split(' ')[0];
+//}
 
-ReactDOM.render(template, appRoot);
+var getFirstName = function getFirstName(fullName) {
+    return fullName.split(' ')[0];
+};
+
+console.log(getFirstName('Joao Luiz'));
