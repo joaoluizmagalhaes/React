73c81891a0bc1199629060c7ad9b3b617f2713be
João Magalhães <joao.magalhaes@newcontent.com.br>
2019-04-01 15:19:00 -0300

    Section 4 - Lesson 2, ES6 Classes: Part 1.

--- indecision-app/public/scripts/app.js
+++ indecision-app/public/scripts/app.js
@@ -1,35 +1,37 @@
 'use strict';
 
-var appRoot = document.getElementById('app');
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
-var visibility = false;
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
-var toggle = function toggle() {
-	visibility = !visibility;
-	render();
-};
+var Person = function () {
+    function Person() {
+        var name = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : 'Anonymous';
+        var age = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : 0;
 
-var render = function render() {
-	var template = React.createElement(
-		'div',
-		null,
-		React.createElement(
-			'h1',
-			null,
-			'Visibility Toggle'
-		),
-		React.createElement(
-			'button',
-			{ onClick: toggle },
-			visibility ? 'Hide Details' : 'Show Details'
-		),
-		visibility && React.createElement(
-			'p',
-			null,
-			'This are the details'
-		)
-	);
-	ReactDOM.render(template, appRoot);
-};
+        _classCallCheck(this, Person);
 
-render();
+        this.name = name;
+        this.age = age;
+    }
+
+    _createClass(Person, [{
+        key: 'getGreeting',
+        value: function getGreeting() {
+            return 'Hi, I am ' + this.name + ' !';
+        }
+    }, {
+        key: 'getDescription',
+        value: function getDescription() {
+            return this.name + ' is ' + this.age + ' year(s) old!';
+        }
+    }]);
+
+    return Person;
+}();
+
+var me = new Person('Joao Luiz', 36);
+
+var other = new Person();
+
+console.log(me.getDescription(), other.getDescription());
