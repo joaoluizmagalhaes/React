c4aeb798a6c7247916fbdcf23fe6ba4eb499e59e
João Magalhães <joao.magalhaes@newcontent.com.br>
2019-04-02 12:04:29 -0300

    Section 4 - Lesson 9, Adding State to Counter App: Part 1.

--- indecision-app/src/playground/counter-example.js
+++ indecision-app/src/playground/counter-example.js
@@ -1,31 +1,66 @@
+class Counter extends React.Component {
+	constructor(props) {
+		super(props);
+		this.handleAddOne = this.handleAddOne.bind(this);
+		this.handleMinusOne = this.handleMinusOne.bind(this);
+		this.handleReset = this.handleReset.bind(this);
+	}
 
-let count = 0;
-const addOne = () => {
-	count++;
-	renderCounterApp();
+	handleAddOne() {
+		console.log('handleAddOne');
+	}
+
+	handleMinusOne() {
+		console.log('handleMinusOne');
+	}
+
+	handleReset() {
+		console.log('handleReset');
+
+	}
+
+	render() {
+		return (
+			<div>
+				<h1>Count: </h1>
+				<button onClick={this.handleAddOne}>+1</button>
+				<button onClick={this.handleMinusOne}>-1</button>
+				<button onClick={this.handleReset}>Reset</button>
+			</div>
+		);
+	}
 }
-const minusOne = () => {
-	count--;
-	renderCounterApp();
-}
-const reset = () => {
-	count = 0;
-	renderCounterApp();
-}
+
+ReactDOM.render(<Counter />, document.getElementById('app'));
+
+
+// let count = 0;
+// const addOne = () => {
+// 	count++;
+// 	renderCounterApp();
+// }
+// const minusOne = () => {
+// 	count--;
+// 	renderCounterApp();
+// }
+// const reset = () => {
+// 	count = 0;
+// 	renderCounterApp();
+// }
 
 
 
-const renderCounterApp = () => {
-	const templateTwo = (
-		<div>
-			<h1>Count: {count}</h1>
-			<button onClick={addOne}>+1</button>
-			<button onClick={minusOne}>-1</button>
-			<button onClick={reset}>Reset</button>		
-		</div>
-	);
+// const renderCounterApp = () => {
+// 	const templateTwo = (
+// 		<div>
+// 			<h1>Count: {count}</h1>
+// 			<button onClick={addOne}>+1</button>
+// 			<button onClick={minusOne}>-1</button>
+// 			<button onClick={reset}>Reset</button>		
+// 		</div>
+// 	);
 
-	ReactDOM.render(templateTwo, appRoot);
-};
+// 	ReactDOM.render(templateTwo, appRoot);
+// };
 
-renderCounterApp();
+// renderCounterApp();
