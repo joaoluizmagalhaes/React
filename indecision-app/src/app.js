e8960c522ad153e4e7a81e8755981b5eec8db1e7
João Magalhães <joao.magalhaes@newcontent.com.br>
2019-04-15 16:09:04 -0300

    Section 6, One component per file.

--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import IndecisionApp from './components/IndecisionApp';
 
-const template = <p>This is JXS from Webpack</p>;
-
-ReactDOM.render(template, document.getElementById('app'))
+ReactDOM.render(<IndecisionApp />, document.getElementById('app'));
