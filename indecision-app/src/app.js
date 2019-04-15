996a50e4ef06224f6e26adc31f939b203e085236
João Magalhães <joao.magalhaes@newcontent.com.br>
2019-04-15 14:31:46 -0300

    Section 6, Importing NPM modules.

--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -1,11 +1,6 @@
-import './utils.js';
-import subtract, { square, add } from './utils.js';
+import React from 'react';
+import ReactDOM from 'react-dom';
 
+const template = React.createElement('p', {}, 'testing 134');
 
-console.log('app.js is running!');
-
-// console.log(square(4), add(1,3), subtract(200, 145));
-
-import isSenior, { isAdult, canDrink } from './person.js';
-
-console.log(isAdult(17), canDrink(22), isSenior(76));
+ReactDOM.render(template, document.getElementById('app'));
