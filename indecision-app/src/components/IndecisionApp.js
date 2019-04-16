9d5db4c57e3e006d229c2619c2e5b768decafb3c
João Magalhães <joao.magalhaes@newcontent.com.br>
2019-04-16 16:51:54 -0300

    Section 7, Passing Children to component.

--- indecision-app/src/components/IndecisionApp.js
+++ indecision-app/src/components/IndecisionApp.js
@@ -3,6 +3,7 @@ import Header from './Header';
 import AddOption from './AddOption'
 import Action from './Action';
 import Options from './Options';
+import OptionModal from './OptionModal';
 
 export default class IndecisionApp extends React.Component {
 	state = {
@@ -77,6 +78,7 @@ export default class IndecisionApp extends React.Component {
 				<AddOption 
 					handleAddOption={this.handleAddOption}
 				/>
+				<OptionModal />
 			</div>
 		);
 	}
