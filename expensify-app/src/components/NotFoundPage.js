177710be89110c97ac3921858a725064f04e67b1
João Magalhães <joao.magalhaes@newcontent.com.br>
2019-04-18 11:30:12 -0300

    Section 9, Organizing Our Routes.

---
+++ expensify-app/src/components/NotFoundPage.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { notDeepEqual } from 'assert';
+
+const NotFoundPage = () => (
+    <div>
+        404! <Link to="/">Go Home</Link>
+    </div>
+);
+
+export default NotFoundPage;
