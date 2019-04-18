31525a0e11c4f852c51c200b02a7ca5f8a1a45c7
João Magalhães <joao.magalhaes@newcontent.com.br>
2019-04-18 15:09:55 -0300

    Section 10, Dispatching Actions.

--- expensify-app/src/playground/redux-101.js
+++ expensify-app/src/playground/redux-101.js
@@ -1,7 +1,48 @@
 import { createStore } from 'redux';
 
-const store = createStore((state = { count: 0 }) => {
-    return state;
+const store = createStore((state = { count: 0 }, action) => {
+    switch (action.type) {
+        case 'INCREMENT':
+            return {
+                count: state.count + 1
+            };
+        case 'DECREMENT':
+            return {
+                count: state.count - 1
+            };
+        case 'RESET':
+            return {
+                count: 0
+            };
+        default:
+            return state;
+    }
 });
+
+console.log(store.getState());
+
+// Actions - Object that gets sent to the store
+
+// Increment Count
+store.dispatch({
+    type: 'INCREMENT' //CONVENTION USE CAPITAL LETTERS IN REDUX
+});
+
+store.dispatch({
+    type: 'INCREMENT' //CONVENTION USE CAPITAL LETTERS IN REDUX
+});
+
+// Reset
+store.dispatch({
+    type: 'RESET'
+});
+
+// Decrement Count
+store.dispatch({
+    type: 'DECREMENT'
+});
+
+
+
 
 console.log(store.getState());
