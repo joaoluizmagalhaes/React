f243aee5e76baee70d1e1fb3bd0ea49534306789
João Magalhães <joao.magalhaes@newcontent.com.br>
2019-04-23 11:28:44 -0300

    Section 11, Wiring up Edit Expense.

--- expensify-app/src/components/EditExpensePage.js
+++ expensify-app/src/components/EditExpensePage.js
@@ -1,15 +1,27 @@
 import React from 'react';
+import ExpenseForm from './ExpenseForm';
+import { connect } from 'react-redux';
+import { editExpense } from '../actions/expenses';
 
 const EditExpensePage = (props) => {
-
-    console.log(props);
-
     return (
         <div>
-            Editing the expense with the id of {props.match.params.id}
+            <ExpenseForm 
+                expense={props.expense}
+                onSubmit={(expense) => {
+                    props.dispatch(editExpense(props.expense.id, expense));
+                    props.history.push('/');
+                    console.log('updated', expense);
+                }}
+            />
         </div>
     );
-    
 };
 
-export default EditExpensePage;
+const mapStateToProps = (state, props) => {
+    return {
+        expense: state.expenses.find((expense) => expense.id === props.match.params.id )
+    };
+};  
+
+export default connect(mapStateToProps)(EditExpensePage);
--- expensify-app/src/components/ExpenseListItem.js
+++ expensify-app/src/components/ExpenseListItem.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { removeExpense } from '../actions/expenses';
+import { Link } from 'react-router-dom';
 
 const ExpenseListItem = ({ id, dispatch, description, amount, createdAt }) => (
     <div>
-        <h3>{description}</h3>
+        <Link to={`./edit/${id}`}>
+            <h3>{description}</h3>
+        </Link>
         <p>{amount} - {createdAt}</p>
         <button onClick={() => {
             dispatch(removeExpense({ id }))
