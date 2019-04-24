8ffeeaaf19de992dbff180fe06f8bd37bea39811
João Magalhães <joao.magalhaes@newcontent.com.br>
2019-04-24 15:55:48 -0300

    Section 12, Testing Expsnes Action Generators.

---
+++ expensify-app/src/tests/actions/expenses.test.js
@@ -0,0 +1,44 @@
+import { addExpenses, editExpense, removeExpense } from '../../actions/expenses';
+
+test('should setup remove expense action object', () => {
+    const action = removeExpense({ id: '123abc'});
+
+    expect(action).toEqual({
+        type: 'REMOVE_EXPENSE',
+        id: '123abc'
+    });
+});
+
+test('should setup edit expense action object', () => {
+    const action = editExpense( '123abc', { note: 'New note value' });
+
+    expect(action).toEqual({
+        type: 'EDIT_EXPENSE',
+        id: '123abc',
+        updates: {
+            note: 'New note value'
+        }
+    });
+});
+
+test('should setup add expense action object with provided values', () => {
+    const expenseData = {
+        description:  'Rent',
+        amount: 109500,
+        createdAt: 1000,
+        note: 'This was the last month rent'
+    };
+    const action = addExpenses(expenseData);
+
+    expect(action).toEqual({
+        type: 'ADD_EXPENSE',
+        expense: {
+            ...expenseData,
+            id: expect.any(String)
+        }
+    });
+});
+
+test('should setup add expense action object with default values', () => {
+
+});
